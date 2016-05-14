ac6435b215c5c870370cc0ea6dc2a3578eab2643
Ali Koba <avk013@gmail.com>
2016-05-14 10:48:37 +0300

    v2

--- move_pic/move_pic/Form1.cs
+++ move_pic/move_pic/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,57 +10,71 @@ using System.Windows.Forms;
 
 namespace move_pic
 {
-    class pic
-    {
-        public string Name;
-        public Form forma;
-        public PictureBox picbox;
-        public int x, y;
-        public enum Direction
-        { Right, Left, Up, Down, None };
-        Direction direction=Direction.None;
-        ////
-        public void RotatePicture(PictureBox pictureBox)
-        {
-            Image flipImage = pictureBox.Image;
-            flipImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
-            pictureBox.Image = flipImage;
-        }
-        ///      
-        public void gopict(PictureBox pictureBox, Direction direction = Direction.None, int shag1 = 0)
-        {
-            if (pictureBox.Left <= 0) { direction = Direction.Right; }
-            
-            if (pictureBox.Left >= forma.Size.Width - pictureBox.Width) { direction = Direction.Left; }
-            if (pictureBox.Top <= 0) { direction = Direction.Down; }
-            if (pictureBox.Top >= forma.Size.Height - pictureBox.Height) { direction = Direction.Up; }
-            switch (direction)
-            {
-                case Direction.Right: if (x < 0) RotatePicture(pictureBox); x = shag1; break;
-                case Direction.Left: if (x > 0) RotatePicture(pictureBox); x = -shag1; break;
-                case Direction.Up: y = -shag1; break;
-                case Direction.Down: y = shag1; break;
-                case Direction.None: break;
-            }
-            pictureBox.Left += x; pictureBox.Top += y;
-        }
-
-        ///
-    }
+   
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+ /// 
+pic a1 = new pic();
+pic a2 = new pic();
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pic a1=new pic();
-            a1.Name = "asd";
-            a1.picbox = pictureBox1;
-            a1.forma = this;
-            
+            label1.Text += "+";
+        }
+        class pic
+        {
+            public Form forma;
+            public PictureBox pictureBox;
+            public int x=10,shag1=10, y=10,mi;
+            public enum Direction  { Right, Left, Up, Down, None };
+            public Direction direction = Direction.None;
+            ////
+            public void RotatePicture(PictureBox pictureBox)
+            {   Image flipImage = pictureBox.Image;
+                flipImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox.Image = flipImage;}
+            ///      
+            //public void gopict(PictureBox pictureBox, Direction direction = Direction.None, int shag1 = 10)
+            public void gopict(Direction direction = Direction.None)
+            {
+              
+                mi++;
+                if (pictureBox.Left <= 0) { direction = Direction.Right; }
+
+                if (pictureBox.Left >= forma.Size.Width - pictureBox.Width) { direction = Direction.Left; }
+                if (pictureBox.Top <= 0) { direction = Direction.Down; }
+                if (pictureBox.Top >= forma.Size.Height - pictureBox.Height) { direction = Direction.Up; }
+                switch (direction)
+                {
+                    case Direction.Right: if (x < 0) RotatePicture(pictureBox); x = shag1; break;
+                    case Direction.Left: if (x > 0) RotatePicture(pictureBox); x = -shag1; break;
+                    case Direction.Up: y = -shag1; break;
+                    case Direction.Down: y = shag1; break;
+                    case Direction.None: break;
+                }
+                pictureBox.Left += x; pictureBox.Top += y;
+            }
+
+            ///
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a1.gopict();
+            a2.gopict();
+            label1.Text =a1.mi.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            a1.forma = a2.forma=this;
+            a1.pictureBox= pictureBox1;
+            a2.pictureBox  = pictureBox2;
+        
         }
     }
 }
